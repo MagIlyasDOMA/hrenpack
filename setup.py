af18295549572c811082b3f5fe22a850f43959f2
Маг Ильяс DOMA <magilyas.doma.09@list.ru>
2025-11-22 17:55:02 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,11 @@ req = open('requirements.txt').read().split('\n')
 
 setup(
     name='hrenpack',
-    version='1.1.1',
-    author_email='hrenpack@mail.ru',
+    version='2.0.0',
+    author_email='magilyas.doma.09@list.ru',
     author='Маг Ильяс DOMA (MagIlyas_DOMA)',
     description=desc,
     license='BSD 3-Clause License',
-    long_description=open('README_PIP.md', encoding='utf-8').read(),
-    long_description_content_type='text/markdown',
     url='https://github.com/MagIlyas-DOMA/hrenpack',
     packages=find_packages(),
     classifiers=[
