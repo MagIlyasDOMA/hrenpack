6f6c016765571fefaf8346cf682fd92fa751f824
Маг Ильяс DOMA <magilyas.doma.09@list.ru>
2026-02-10 13:05:47 +0300

    2.5.9

--- hrenpack/__init__.py
+++ hrenpack/__init__.py
@@ -1,5 +1,6 @@
 ﻿import os, warnings
 from hrenpack.classes import range_plus
+from hrenpack.decorators import deprecated
 from hrenpack.functionwork import empty_function
 from hrenpack.strwork import randstr
 from hrenpack.listwork import split_list
@@ -12,7 +13,7 @@ def credits():
     print("Licensed under MIT (https://github.com/MagIlyasDOMA/hrenpack/blob/main/LICENSE)")
 
 
-__version__ = '2.5.5'
+__version__ = '2.5.9'
 
 
 def print_message_set(value: bool = False):
@@ -46,6 +47,10 @@ def write(path, text):
     file.close()
 
 
+def null():
+    warnings.warn('This function will be removed in version 3.0.0, use functionwork.empty_function instead', DeprecationWarning, 2)
+
+
 def switch(variable, case: dict, default=empty_function):
     for key in case:
         func = case[key]
@@ -61,6 +66,28 @@ def bincode_generator(length: int, isInt: bool = False):
     for i in range(length):
         bincode = bincode + randstr(0, 1)
     return int(bincode) if isInt else bincode
+
+
+def show_help(path_of_document: str, path: str = ''):
+    warnings.warn('This function will be removed in version 3.0.0', DeprecationWarning, 2)
+    def return_text(pod):
+        document = of_utf8(path_of_document)
+        data = document.read()
+        document.close()
+        return data
+
+    text = return_text(path_of_document)
+
+    if path:
+        if not os.path.isfile(path):
+            raise FileExistsError(
+                f'[WinError 183] Невозможно создать новый файл, так как он уже существует: {path}')
+        else:
+            file = of_utf8(path, 'w')
+            file.write(text)
+            file.close()
+    else:
+        print(text)
 
 
 def switch_return(variable, case: dict, default=None):
@@ -83,6 +110,19 @@ def who_called_me():
     current_frame = inspect.currentframe()
     calling_frame = current_frame.f_back
     return inspect.getfile(calling_frame)
+
+
+def get_resource(path: str):
+    """Вызывает ресурс hrenpack. Работает только, если вызывать внутри пакета hrenpack
+    :arg path: Принимаются только пути, относительные \\hrenpack\\resources\\
+    """
+    warnings.warn('This function will be removed in version 3.0.0', DeprecationWarning, stacklevel=2)
+    python_path = who_called_me()
+    python_list = python_path.split('\\')
+    if 'hrenpack' not in python_list:
+        raise NotADirectoryError
+    hrenpack_index = python_list.index('hrenpack')
+    return '\\'.join(python_list[:hrenpack_index + 1]) + '\\resources\\' + path
 
 
 def one_return(count: int, value=None):
