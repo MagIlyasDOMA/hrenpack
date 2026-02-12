d993bb550b9f72fa4653adb03927dcb7ba19368c
Маг Ильяс DOMA <magilyas.doma.09@list.ru>
2026-02-12 16:15:52 +0300

    Update screen.py

--- hrenpack/framework/Pyside6/screen.py
+++ hrenpack/framework/Pyside6/screen.py
@@ -1,11 +1,22 @@
 from typing import Optional
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtWidgets import QStackedWidget
+from PySide6.QtWidgets import QStackedWidget, QWidget
 
 
-class QScreenWidget(QStackedWidget):
+class QScreenManager(QStackedWidget):
     def __init__(self, parent, *, currentIndex: Optional[int] = None, count: Optional[int] = None):
         super().__init__(parent, currentIndex=currentIndex, count=count)
         self._ui_loader = QUiLoader()
 
-    def
+    def add_screen(self, screen):
+        if isinstance(screen, type):
+            if not issubclass(screen, QWidget):
+                class Screen(QWidget, screen):
+                    pass
+            else:
+                Screen = screen
+            screen = Screen()
+        if not isinstance(screen, QWidget):
+            raise TypeError("screen is not a QWidget")
+        self.addWidget(screen)
+
