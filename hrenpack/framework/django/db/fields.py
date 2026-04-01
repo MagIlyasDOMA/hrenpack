5627b4ebd0b60927f8c4f56addde1b3740418aec
Маг Ильяс DOMA <magilyas.doma.09@list.ru>
2026-04-01 20:41:20 +0300

    Remove DirnameLookup and simplify FilePathField
    Delete the custom Dirname/DirnameLookup implementation and stop registering it on FilePathField. The lookup module hrenpack/framework/django/db/lookup.py was removed and FilePathField no longer imports or registers DirnameLookup (now a pass-through placeholder). Update any code that relied on the __dirname lookup accordingly.

--- hrenpack/framework/django/db/fields.py
+++ hrenpack/framework/django/db/fields.py
@@ -1,8 +1,5 @@
 from django.db import models
-from .lookup import DirnameLookup
 
 
 class FilePathField(models.CharField):
-    def __init__(self, *args, db_collation=None, **kwargs):
-        super().__init__(*args, db_collation=db_collation, **kwargs)
-        self.register_lookup(DirnameLookup)
+    pass
